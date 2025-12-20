9f64c22c6257894f41c525ce903ffa82686667fc
Mor <toxice290@gmail.com>
2025-12-20 13:32:08 +0200

    created a base to continue from

--- client.py
+++ client.py
@@ -1,64 +1,18 @@
-import socket
 import json
-import time
-import socket as sock
-from typing import Any
+import socket
 
-# Default
-IP_ADDRESS = "127.0.0.1"
-PORT = 5555
-BUFFER_SIZE = 4096
+class Window:
+    def __init__(self, buffer: list, size:int):
+        self.window = buffer
+        self.window_size = size
 
-def init_client():
-    client_socket = sock.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Client:
+    def __init__(self, host, port, timer=0, window=5,):
+        self.host = host
+        self.port = port
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.window = window
 
-    #---- 3 Way Handshake ----
-    print("Connecting to the Server, Sending [SYN]")
-    client_socket.sendall(init_handshake())
-
-
-def init_handshake() -> bytes:
-    """
-    starting the 3 Way Handshake Process
-    :return: the [SYN] request in Binary Foramt
-    """
-    data = {"type": "SYN"}
-    return msg_to_json(data)
-
-
-def send_ack(client: sock.socket, segment: dict):
-    ack_number = segment.get('ack')
-
-    data = {
-        "type": "ACK",
-        "ack_number": ack_number
-    }
-    client.sendall(msg_to_json(data))
-
-def msg_to_json(message: dict) -> bytes:
-    """
-    convert JSON to Bytes
-    :param message: message
-    :return: a Byte Array of made from the JSON dictionary
-    """
-    return (json.dumps(message, ensure_ascii=False) + "\n").encode(encoding='uft-8')
-
-def to_json(msg: bytes) -> dict:
-    return json.loads(msg.decode(encoding='utf-8'))
-
-
-def handle_response(client: sock.socket) -> dict:
-    buffer = b""
-    while True:
-        raw_chunk = client.recv(1024)
-        if not raw_chunk:
-            return {"type": "FIN"}
-        buffer += raw_chunk
-
-        if b"\n" in buffer:
-            line, _, _ = buffer.partition(b"\n")
-            return to_json(line)
-
-
-def main():
-    init_client()
+    def init_client(self):
+        with self.client_socket as s:
+            s.connect((self.host, self.port))
--- server.py
+++ server.py
@@ -1,61 +1,27 @@
-import socket
 import json
-import time
-import socket as sock
+import socket
 
-# Default
-IP_ADDRESS = "127.0.0.1"
+# Default Parameters
+HOST = "127.0.0.1"
 PORT = 5555
-BUFFER_SIZE = 4096
 
-def init_server():
-    """
-    initialize the TCP Server
-    Accept a [SYN] and Send a [SYN,ACK]
-    """
-    server_socket = sock.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((IP_ADDRESS, PORT))
-    server_socket.listen(1)
-    print(f"Server listening on {IP_ADDRESS}:{PORT}")
-
-    while True:
-        connection, address = server_socket.accept()
-
-        # 3 Way Handshake:
-        #--- Step 1: Accept [SYN] and send [SYN,ACK]
-        raw_data = connection.recv(BUFFER_SIZE)
-        message = json.loads(raw_data.decode("utf-8"))
-
-        # if message['type'] == "SYN":
-        #     print("[SYN] Received from Client, Sending [SYN,ACK]")
-        #     handle_response(server_socket, {"type": "SYN-ACK"})
-        match message.get('type'):
-            case "SYN":
-                print("got SYN Request, Sending [SYN,ACK]")
+class server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-
-def to_json(msg: bytes) -> dict:
-    return json.loads(msg.decode(encoding='utf-8'))
-
-
-
-def handle_response(server_socket: sock.socket, message):
-    """
-    :param server_socket: a TCP Socket for sending data over the network
-    :param message: the message itself, JSON based
-    """
-    data = json.dumps(message)
-    server_socket.sendall(data.encode(encoding='utf-8'))
-
-def build_response(payload):
-        print()
+    # 1st step - connect to a TCP socket
+    def init_socket(self):
+        """
+        init TCP socket
+        :param self: TCP Socket
+        """
+        with self.server_socket as s:
+            s.bind((self.host, self.port))
+            s.listen(1)
+            print(f"listen on {self.host}:{self.port}")
+            conn, address = s.accept()
 
 
-
-def main():
-    init_server()
-
-
-if __name__ == "__main__":
-    main()
