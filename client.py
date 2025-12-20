14ff9168ad5baa0d1f6534c66956a89da107ac98
Mor <toxice290@gmail.com>
2025-12-20 14:33:30 +0200

    Handshake

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import json
 import socket
+from typing import Any
+
 
 class Window:
     def __init__(self, buffer: list, size:int):
@@ -16,3 +18,21 @@ class ReliableClient:
     def init_client(self):
         with self.client_socket as s:
             s.connect((self.host, self.port))
+            print("Sending SYN Request...")
+            s.sendall(b"SYN\n")
+
+            response = s.recv(1024).decode().strip()
+            if response == "SYN/ACK":
+                print("Accepted SYN/ACK from server, Sending ACK")
+                s.sendall(b"ACK")
+            else:
+                print(f"Unexpected Response: {response}")
+
+
+
+def __main__():
+    client = ReliableClient("127.0.0.1", 5555)
+    client.init_client()
+
+if __name__ == "__main__":
+    __main__()
--- server.py
+++ server.py
@@ -4,20 +4,43 @@ import socket
 # Default Parameters
 HOST = "127.0.0.1"
 PORT = 5555
-BUFFER_SIZE = 1024
 
 class Reliable_server:
-    def __init__(self, host, port, conn):
+    def __init__(self, host, port):
         self.host = host
         self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         with self.server_socket as s:
             s.bind((self.host, self.port))
             s.listen(1)
             print(f"listen on {self.host}:{self.port}")
-            self.conn, address = s.accept()
+            self.conn, self.address = s.accept()
 
     def accept_connection(self):
-        with self.server_socket as s:
-            s.recv
+        with self.conn as server:
+            raw_buffer = b""
+            while True:
+                chunk = server.recv(1024)
+                if not chunk:
+                    print("Client Disconnected")
+                    break
+                raw_buffer += chunk
 
+                while b"\n" in raw_buffer:
+                    line, _, raw_buffer = raw_buffer.partition(b"\n")
+
+                    data = line.decode().strip()
+                    if data == "SYN":
+                        print(f"Accepted SYN from {self.address}, Sending SYN/ACK")
+                        server.sendall(b"SYN/ACK\n")
+
+                    elif data == "ACK":
+                        print("Handshake Established (ACK received)")
+
+def __main__():
+    server = Reliable_server("127.0.0.1", 5555)
+    server.accept_connection()
+
+if __name__ == "__main__":
+    __main__()
